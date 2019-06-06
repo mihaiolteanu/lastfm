06dff273309813fc2464374896cf84c025100c88
mihaiolteanu <mihai_olteanu@fastmail.fm>
2019-06-06 08:22:52 +0300

    Export authorize user

--- package.lisp
+++ package.lisp
@@ -1,7 +1,8 @@
 ;;;; package.lisp
 
 (defpackage :lastfm
-  (:use :cl :drakma :plump :lquery :defmemo)
-  ;; All exported functions are created and exported "in-code" based on the
-  ;; *methods* table
-)
+  (:use :cl :drakma :plump :lquery :defmemo)  
+  (:export authorize-user
+           ;; All the other exported functions are created and exported
+           ;; "in-code" based on the *methods* table
+           ))
