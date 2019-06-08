9c3a5510c2b4f4656526cf54756ded6c58019519
mihaiolteanu <mihai_olteanu@fastmail.fm>
2019-06-08 13:21:14 +0300

    Update the exported symbols

--- package.lisp
+++ package.lisp
@@ -6,6 +6,19 @@
   (:import-from :alexandria :random-elt)
   (:import-from :alexandria :circular-list)
   (:export authorize-user
+           ;; Aux functionality
+           song-youtube-url
+           random-artist-song
+           random-similar-artist
+           random-user-loved-song
+           random-tag-song
+           random-tag-artist
+           ;; Generators
+           artist-songs
+           tag-songs
+           user-songs
+           artist-similar-artists-songs
+           tag-similar-artists-songs
            ;; All the other exported functions are created and exported
            ;; "in-code" based on the *methods* table
            ))
